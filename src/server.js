b6c16c029dcd2f50d003adbbe2a4740aea444e60
Yoga Anugrah Pratama <yogaanugrahpsy@gmail.com>
2021-06-28 17:07:01 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+//notes
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
+
+//users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+ 
+const init = async () => {
+  const notesService = new NotesService();
+  const usersService = new UsersService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  await server.register([
+  {
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },  
+  },
+  {
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
+    },
+  },
+]);
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
